cee4eee13df187656081e9953e1cf01f609e11f7
Сергей <seirosso1@gmail.com>
2020-12-24 00:23:44 +0300

    Сокобан. Тестовое задание.

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,57 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
+        boolean rsl = false;
+        for (int i = 0; i < board.length; i++) {
+            if (1 == board[i][i]) {
+                if ((monoHorizontal(board, i) || monoVertical(board, i))) {
+                    rsl = true;
+                    break;
+                }
+            }
+        }
+        return rsl;
+    }
+
+    public static boolean monoHorizontal(int[][] board, int row) {
         boolean rsl = true;
+        for (int i = 0; i < board.length; i++) {
+            if (1 != board[row][i]) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public static boolean monoVertical(int[][] board, int column) {
+        boolean rsl = true;
+        for (int i = 0; i < board.length; i++) {
+            if (1 != board[i][column]) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public static int[] extractDiagonal(int[][] board) {
+        int[] rsl = new int[board.length];
+        for (int i = 0; i < board.length; i++) {
+            rsl[i] = board[i][i];
+        }
         return rsl;
     }
 }
+
+//        public static boolean isWin(char[][] board) {
+//            boolean result = false;
+//            for (int i = 0; i < board.length; i++) {
+//                if ('X' == board[i][i]) {
+//                    if ((monoHorizontal(board, i) || monoVertical(board, i))) {
+//                        result = true;
+//                        break;
+//                    }
+//                }
+//            }
+//}
